01d1a82da53a81cc9ffd42aefab62899cf815eb7
mahmoud <mahmoudshahim5@gmail.com>
2023-05-23 21:35:17 +0300

    Initial commit

--- src/print.java
+++ src/print.java
@@ -3,5 +3,6 @@ public class print {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");}
+    //asdasd
 
 }
